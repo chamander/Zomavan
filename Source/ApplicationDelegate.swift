9151c087da442c1f22cb4e30c56e6e92c7636c2e
Gavan Chan <chan.gavan+github@gmail.com>
2018-11-03 01:12:06 +1100

    Display a table view for restaurant list

--- Source/ApplicationDelegate.swift
+++ Source/ApplicationDelegate.swift
@@ -4,25 +4,25 @@ import UIKit
 final class ApplicationDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+
     private var applicationController: ZomavanController!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+
         defer { self.window?.makeKeyAndVisible() }
-        
+
         let (window, applicationController) = self.initialiseApplication()
-        
+
         self.window = window
         self.applicationController = applicationController
         return true
     }
-    
+
     private func initialiseApplication() -> (window: UIWindow, controller: ZomavanController) {
-        
+
         let window: UIWindow = UIWindow(frame: UIScreen.main.bounds)
         let controller: ZomavanController = ZomavanController()
-        
+
         window.rootViewController = controller.viewController
         return (window, controller)
     }
--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -6,9 +6,9 @@ import Quick
 final class ZomavanTests: QuickSpec {
 
     override func spec() {
-     
+
         describe("a test") {
-            
+
             it("passes") {
                 expect(true).to(beTruthy())
             }
